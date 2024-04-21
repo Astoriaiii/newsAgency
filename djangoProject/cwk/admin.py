3e8d977fccd8f111a7d9f6a25b46c97bc6938790
Astoriaiii <95459670+Astoriaiii@users.noreply.github.com>
2024-04-21 16:00:43 +0800

    Delete djangoProject directory

--- djangoProject/cwk/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import StoryModel
-from .models import user
-
-admin.site.register(StoryModel)
-admin.site.register(user)
-
